89de45a6fc7053207f985242370125714e6bfacc
David Wilson <dw@botanicus.net>
2014-01-22 00:41:30 +0000

    Cleanups

--- bisect-test2.py
+++ bisect-test2.py
@@ -7,53 +7,46 @@ import zlib
 
 import sortedfile
 
-maxcomp = 8
-target = 8000
+maxcomp = 6
+target = 7500
 
-lines = file('/usr/share/dict/words').readlines()
-
-line_count = len(lines)
-line_sum = sum(map(len, lines))
-line_avg = float(line_sum) / line_count
-ubound = int(target / line_avg) * maxcomp
+lines = []
+line_sum = 0
+for line in file('/Users/dmw/messages'):
+    lines.append(line)
+    line_sum += len(line)
+    line_avg = float(line_sum) / len(lines)
+    if ((len(lines) * line_avg) / maxcomp) > target:
+        break
 
 print 'input len:', len(lines)
 print 'max comp:', maxcomp
-print 'ubound:', ubound
-print 'input avgsize:', sum(map(len, lines)) / float(len(lines))
-print 'input steps:', math.log(len(lines), 2), 'all /', math.log(ubound,2), 'ubound'
+print 'input avgsize:', line_avg
+print 'input steps:', math.log(len(lines), 2)
 print 'target size:', target
 print
 
 steps = [0]
+cac = {}
 
 def compo(i):
-    dat = zlib.compress(''.join(lines[:i]))
-    actual = len(dat)
-    ratio = actual/float(target)
-    out = (ratio, dat)
-    print[i, actual, ratio]
-    steps[0]+=1
-    return out
-
+    output = zlib.compress(''.join(lines[:i]))
+    cac[i] = output
+    print [i, len(output) - target]
+    steps[0] += 1
+    return len(output) - target
 
 
 t0 = time.time()
 
-doink = sortedfile.bisect_func_right((1.0,), 1, ubound, compo)
-
-maxlines, (ratio, output) = doink
-if maxlines and (ratio > 1.0):
-    maxlines -= 1
-    ratio, output = compo(maxlines)
+maxlines, actual = sortedfile.bisect_func_right(0, 1, len(lines), compo)
+maxlines -= 1
+output = cac[maxlines]
 
 print
 print 'time taken:', 1000 * (time.time() - t0)
 print 'output steps:', steps
 print 'maxlines:', maxlines
 print 'maxlines len:', sum(map(len, lines[:maxlines]))
-print 'target ratio:', ratio
 print 'compression ratio:', sum(map(len, lines[:maxlines])) / float(len(output))
 print 'outlen:', len(output)
-
-#print compo(50, (15000,))
