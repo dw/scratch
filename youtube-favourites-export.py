4f3221a4374296ad9795bb676eb59aa065945340
David Wilson <dw@botanicus.net>
2014-03-15 20:15:56 +0000

    export yt favs

---
+++ youtube-favourites-export.py
@@ -0,0 +1,21 @@
+
+import gdata.youtube.client
+
+
+client = gdata.youtube.client.YouTubeClient()
+client.client_login('email@gmail.com', 'password', 'exporter')
+
+entries = []
+uri = 'https://gdata.youtube.com/feeds/api/users/default/favorites'
+while True:
+    print 'Fetch', uri
+    feed = client.get_videos(uri=uri, **{'max-results': 50})
+    entries += feed.entry
+    if not feed.get_next_link():
+        break
+    uri = feed.get_next_link().href
+
+feed.entry = entries
+print 'total', len(entries)
+with open('youtube-favorites.xml', 'w') as fp:
+    fp.write(feed.to_string())
