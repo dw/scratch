d558f7bc11f5d8b51d619bd1fe00a9ff69be382a
David Wilson <dw@botanicus.net>
2014-03-15 20:23:05 +0000

    do subs too

--- youtube-favourites-export.py
+++ youtube-favourites-export.py
@@ -19,3 +19,22 @@ feed.entry = entries
 print 'total', len(entries)
 with open('youtube-favorites.xml', 'w') as fp:
     fp.write(feed.to_string())
+
+
+# get subs
+#
+
+entries = []
+uri = 'https://gdata.youtube.com/feeds/api/users/default/subscriptions'
+while True:
+    print 'Fetch', uri
+    feed = client.get_feed(uri=uri, **{'max-results': 50})
+    entries += feed.entry
+    if not feed.get_next_link():
+        break
+    uri = feed.get_next_link().href
+
+feed.entry = entries
+print 'total', len(entries)
+with open('youtube-subs.xml', 'w') as fp:
+    fp.write(feed.to_string())
